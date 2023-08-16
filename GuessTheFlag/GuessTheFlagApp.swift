04429d200550816132426e1be582e118fb8008e2
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-16 17:57:06 +0900

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Lyu Hiroyama on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
